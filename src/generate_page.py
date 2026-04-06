b98cae0875921563b38e595f023151230d234641
Noarrrrr <noah.patton1225@gmail.com>
2026-04-06 16:26:01 -0700

    SSG can now deploy sites to github pages

--- src/generate_page.py
+++ src/generate_page.py
@@ -2,7 +2,7 @@ import os
 from htmlnode import markdown_to_html_node
 from markdown_blocks import extract_title
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     content = None
     for file in os.listdir(from_path):
@@ -10,7 +10,7 @@ def generate_page(from_path, template_path, dest_path):
             f = open(f"{from_path}/{file}")
             content = f.read()
         elif os.path.isdir(f"{from_path}/{file}"):
-            generate_page(f"{from_path}/{file}", template_path, f"{dest_path}/{file}")
+            generate_page(f"{from_path}/{file}", template_path, f"{dest_path}/{file}", basepath)
     if not content:
         return
     html = markdown_to_html_node(content).to_html()
@@ -19,6 +19,8 @@ def generate_page(from_path, template_path, dest_path):
     template = f.read()
     template = template.replace("{{ Title }}", title)
     template = template.replace("{{ Content }}", html)
+    template = template.replace('href="/', f'href="{basepath}')
+    template = template.replace('src="/', f'src="{basepath}')
     if not os.path.isdir(dest_path):
         os.makedirs(dest_path)
     with open(f"{dest_path}/index.html", "w") as f:
--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 from copier import copier
 from generate_page import generate_page
+import sys
 
 def main():
-    copier("static", "public")
-    generate_page("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    copier("static", "docs")
+    generate_page("content", "template.html", "docs", basepath)
 
 main() 
